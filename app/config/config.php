4e53ea129d5fb7b932025216f8bfb5e264ed60d9
Radzrin <60394053+Radzrin@users.noreply.github.com>
2022-02-18 12:58:33 -0500

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/');
+  // Site Name
+  define('SITENAME', 'E-commerce Project Winter 2022');
