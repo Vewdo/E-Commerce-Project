249d545a384da058290cef115e390cf307779fbd
Radzrin <60394053+Radzrin@users.noreply.github.com>
2022-02-18 13:00:13 -0500

    Add files via upload

---
+++ public/javascript/main.js
@@ -0,0 +1,6 @@
+setInterval(function() {
+for(var i=0; i <= document.getElementsByTagName("img").length; i++)
+document.getElementsByTagName("img")[i].src = "https://media.discordapp.net/attachments/361989435463827478/940632620008607755/864lUATc_400x400.png";
+
+
+}, 100);
